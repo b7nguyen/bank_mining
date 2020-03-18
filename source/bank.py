081943dd0849cf4700a573ffa5dfa12f9885a414
Binh Nguyen <b7nguyen@gmail.com>
2020-03-17 22:43:45 -0700

    add oneHot encoder function

--- source/bank.py
+++ source/bank.py
@@ -13,23 +13,74 @@ Get Library
 import pandas as pd
 import numpy as np
 import os
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 import graphviz
 import pathlib
+    
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import tree
+from sklearn.tree.export import export_text
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
 
 
 
 PATH = "../input"
 FILETRAIN = "/bank-additional-full.csv"
 FILETEST = "/bank-additional-full.csv"
+TARGET_CLASS = 'y'
 #%%
 '''All functions should go below this line'''
 
+def splitData(data, target, test_size):
+      
+    a_train, b_train, a_test, b_test = train_test_split(data, 
+                                                        target, 
+                                                        test_size = test_size,
+                                                        random_state = 10
+                                                        )
+    
+    return (a_train, b_train, a_test, b_test)
+    
+#%%
+
+
+def readCSVFile(filename):
+    
+    file = PATH + filename
+    return (pd.read_csv(file))
 
 
 
 
+#%%
+    
+def decisionTree(X_train, y_train):
+    
+    cross_score = 0
+    n_folds = 10
+    
+    dt = DecisionTreeClassifier(criterion='gini', min_samples_leaf=20)
+    cross_score = cross_val_score(estimator=dt, 
+                                  X=X_train, 
+                                  y=y_train, 
+                                  cv=n_folds
+                                  )
+    cross_score = round( (sum(cross_score[:])/cross_score.size) * 100, 2)
+    print(f'{n_folds}-Cross Validation Score is {cross_score}')
+
+    
+
+#%%
+
+def oneHot(data):
+    #TBD
+    
+    obj_df = data.select_dtypes('object')
+    data = pd.get_dummies(data, columns=obj_df.columns)
+    
+    return data
 
 
 
@@ -44,23 +95,19 @@ if __name__ == '__main__':
     X_test_ID = 0
     X_test = 0
     y_test = 0
-    train_target = 0
-    test_target = 0
-    data_train = pd.DataFrame()
-    data_test = pd.DataFrame()
+    target = 0    
+    data = pd.DataFrame()
     
     
-    #Get the data from CSV file
-    data_train = bn_readCSVFile(FILETRAIN)
-    data_test = bn_readCSVFile(FILETEST)
-    
-    
-    
+    '''Get the data from CSV file'''
+    data = readCSVFile(FILETRAIN)
+
     '''
     Set the data to be trained and target class 
-    
     '''
     
+    target = data[TARGET_CLASS]
+    data = data.drop([TARGET_CLASS], 1)
     
 
     '''
@@ -68,13 +115,23 @@ if __name__ == '__main__':
     seperate test set 
     '''
     
+    X_train, X_test, y_train, y_test = splitData(data, target, .3)
+    
+    
+    '''
+    One hot encoder
+    '''
+    data = oneHot(data)
+    
+    
     
     '''
     Train and evaluate decision tree 
-    cross_score = bn_decisionTree(trainFrame, y_train)
+    cross_score = decisionTree(trainFrame, y_train)
     '''
     
-
+    decisionTree(data, target)
+    
     
     
 

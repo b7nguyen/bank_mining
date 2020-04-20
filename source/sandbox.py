3662b7d623ddcd70c48145da2c6d7c2f1f865e25
Binh Nguyen <b7nguyen@gmail.com>
2020-04-20 14:09:45 -0700

    Classify menu cleaned

--- source/sandbox.py
+++ source/sandbox.py
@@ -7,25 +7,19 @@ Created on Thu Apr  9 14:07:46 2020
 """
 
 from datetime import datetime
+import pandas as pd
 
 import math
 import random
 
-
+import numpy as np
 from sklearn import tree
 from sklearn.datasets import load_iris
 from sklearn import tree
 
 
 #%%
+df = pd.DataFrame([['bar', 'one'], ['bar', 'two'],
+                  ['foo', 'one'], ['foo', 'two']],
+                  columns=['first', 'second'])
 
-X, y = load_iris(return_X_y = True)
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
-
-#%%
-
-tree.plot_tree(clf.fit(X, y)) 
-
-
-a = input('press enter')
